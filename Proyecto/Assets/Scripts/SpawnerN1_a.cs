c0573f323146f27391a9810ec3e42b25b603058e
DiegoTorresP <1220100050@alumnos.utng.edu.mx>
2023-10-30 18:28:06 -0600

    Controlador de Enemigos
    Controlador de Enemigos terminado. Los enemigos Spawnean, los enemigos siguen al jugador.

---
+++ Proyecto/Assets/Scripts/SpawnerN1_a.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpawnerN1_a : MonoBehaviour
+{
+    public GameObject enemyPrefab; // El GameObject del enemigo que has creado
+    public Transform spawnPoint; // El punto de generación
+    private int cantidadDePrefabs = 20;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemiesWithDelay());
+    }
+
+    IEnumerator SpawnEnemiesWithDelay()
+    {
+        for (int i = 0; i < cantidadDePrefabs; i++)
+        {
+            GameObject newEnemy = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+            yield return new WaitForSeconds(10);
+        }
+    }
+}
